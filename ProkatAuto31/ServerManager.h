214466106d584fae337f4b2d6eaec0a312ca8337
Admin <admin@Admins-Mac.local>
2016-10-21 10:58:18 +0300

    Merge dev->ivan

--- ProkatAuto31/ServerManager.h
+++ ProkatAuto31/ServerManager.h
@@ -30,9 +30,24 @@
                                      onFail:(void(^)(NSError* error, NSInteger statusCode)) failure
                              withCategoryID: (NSNumber*) categoryID;
 
+- (void) getCarWithoutDriverDetailWithTransmissionOnSuccess:(void(^)(NSArray* thisData)) success
+                                                     onFail:(void(^)(NSError* error, NSInteger statusCode)) failure
+                                             withCategoryID: (NSNumber*) categoryID;
+
 - (void) getCarWithDriverDetailOnSuccess:(void(^)(NSArray* thisData)) success
                                      onFail:(void(^)(NSError* error, NSInteger statusCode)) failure
                              withCategoryID: (NSNumber*) categoryID;
+
+- (void) getPlacesOnSuccess:(void(^)(NSArray* thisData)) success
+                                       onFail:(void(^)(NSError* error, NSInteger statusCode)) failure;
+
+- (void) checkCarWithCarId: (NSNumber*) carId withDateFrom: (NSString*) dateFrom withDateTo: (NSString*) dateTo
+                 OnSuccess:(void(^)(NSArray* thisData)) success
+                    onFail:(void(^)(NSError* error, NSInteger statusCode)) failure;
+
+- (void) getCarOptionsOnSuccess:(void(^)(NSArray* thisData)) success
+                         onFail:(void(^)(NSError* error, NSInteger statusCode)) failure;
+
 
 
 @end
--- ProkatAuto31/ServerManager.m
+++ ProkatAuto31/ServerManager.m
@@ -10,7 +10,10 @@
 #import "AFNetworking.h"
 #import "Category.h"
 #import "Car.h"
+#import "Place.h"
+#import "Option.h"
 #import "CarWithDriver.h"
+
 
 @interface ServerManager ()
 
@@ -237,18 +240,14 @@
                      }];
 
 }
-
 - (void) getPlacesOnSuccess:(void(^)(NSArray* thisData)) success
                      onFail:(void(^)(NSError* error, NSInteger statusCode)) failure{
-    
     [self.sessionManager GET:@"services/"
                   parameters:nil
                     progress:nil
                      success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                         
                          NSArray *dictsArray = [responseObject allObjects];
                          NSMutableArray *objectsArray = [NSMutableArray array];
-                         
                          for (NSDictionary* dic in dictsArray) {
                              Place *place = [[Place alloc] initWithServerResponse:dic];
                              [objectsArray addObject:place];
@@ -259,96 +258,73 @@
                      }
                      failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
                          NSLog(@"Error: %@", error);
-                     }];
-}
-
-- (void) checkCarWithCarId: (NSNumber*) carId withDateFrom: (NSString*) dateFrom withDateTo: (NSString*) dateTo
+                     }]; }
+- (void) checkCarWithCarId: (NSNumber*) carId
+              withDateFrom: (NSString*) dateFrom
+                withDateTo: (NSString*) dateTo
                  OnSuccess:(void(^)(NSArray* thisData)) success
                     onFail:(void(^)(NSError* error, NSInteger statusCode)) failure {
-    
     NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
                             carId, @"car",
                             dateFrom, @"date_from",
                             dateTo, @"date_to" , nil];
-    
     [self.sessionManager POST:@"cars/check/"
                    parameters:params
                      progress:nil
-                      success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                          NSArray *dictsArray = [responseObject allObjects];
+                      success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {                           NSArray *dictsArray = [responseObject allObjects];
                           if (success) {
                               success(dictsArray);
-                         
                           }
-                          
                       }
-                      failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                           //NSLog(@"Error: %@", error);
-                          if (failure) {
-                              failure(error, 7);
-                              NSLog(@"hgfhjjgkjhkhjkhkhjkkhjhjk");
+                      failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error)
+     {
+         //NSLog(@"Error: %@", error);
+         if (failure) {
+             failure(error, 7);
+             NSLog(@"hgfhjjgkjhkhjkhkhjkkhjhjk");
+         }
+     }]; } - (void) getCarOptionsOnSuccess:(void(^)(NSArray* thisData)) success
+                                    onFail:(void(^)(NSError* error, NSInteger statusCode)) failure {
+         [self.sessionManager GET:@"options/"
+                       parameters:nil
+                         progress:nil
+                          success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {                          NSArray *dictsArray = [responseObject allObjects];
+                              NSMutableArray *objectsArray = [NSMutableArray array];
+                              for (NSDictionary* dic in dictsArray) {
+                                  Option *option = [[Option alloc] initWithServerResponse:dic];
+                                  [objectsArray addObject:option];
+                              }
+                              if (success) {
+                                  success(objectsArray);
+                              }
                           }
-                      }];
-}
-
-- (void) getCarOptionsOnSuccess:(void(^)(NSArray* thisData)) success
-                         onFail:(void(^)(NSError* error, NSInteger statusCode)) failure {
-    
-    [self.sessionManager GET:@"options/"
-                  parameters:nil
-                    progress:nil
-                     success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                         NSArray *dictsArray = [responseObject allObjects];
-                         NSMutableArray *objectsArray = [NSMutableArray array];
-                         
-                         for (NSDictionary* dic in dictsArray) {
-                             Option *option = [[Option alloc] initWithServerResponse:dic];
-                             [objectsArray addObject:option];
-                         }
-                         if (success) {
-                             success(objectsArray);
-                         }
-
-                         
-                     }
-                     failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                         NSLog(@"Error: %@", error);
-                     }];
-    
-    
-}
-
+                          failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                              NSLog(@"Error: %@", error);
+                          }];
+     }
 - (void) getCarWithDriverDetailOnSuccess:(void(^)(NSArray* thisData)) success
-                                     onFail:(void(^)(NSError* error, NSInteger statusCode)) failure
-                             withCategoryID: (NSNumber*) categoryID {
-    
+                                  onFail:(void(^)(NSError* error, NSInteger statusCode)) failure
+                          withCategoryID: (NSNumber*) categoryID {
     NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:categoryID, @"category", nil];
-    
     [self.sessionManager GET:@"hourly-cars/"
                   parameters:params
                     progress:nil
-                     success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                         //NSLog(@"responseObject cars %@", responseObject);
-                         NSArray *dictsArray = [responseObject objectForKey:@"results"];
-                         NSMutableArray *objectsArray = [NSMutableArray array];
-                         
-                         for (NSDictionary* dic in dictsArray) {
-                             CarWithDriver *car = [[CarWithDriver alloc] initWithServerResponse:dic];
-                             [objectsArray addObject:car];
-                         }
-                         
-                         
-                         if (success) {
-                             success(objectsArray);
-                         }
-                         
-                         
-                     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                         NSLog(@"Error: %@", error);
-                     }];
-    
-    
+                     success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject)
+     {
+         //NSLog(@"responseObject cars %@", responseObject);
+         NSArray *dictsArray = [responseObject objectForKey:@"results"];
+         NSMutableArray *objectsArray = [NSMutableArray array];
+         for (NSDictionary* dic in dictsArray) {
+             CarWithDriver *car = [[CarWithDriver alloc] initWithServerResponse:dic];
+             [objectsArray addObject:car];
+         }
+         if (success) {
+             success(objectsArray);
+         }
+     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error)
+     {
+         NSLog(@"Error: %@", error);
+     }];
 }
-
 
 @end
