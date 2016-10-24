2e6b52933230473740801754e4a408e77135b32d
Admin <admin@Admins-Mac.local>
2016-10-24 16:18:38 +0300

    Picker view was added to order car with driver controller

--- ProkatAuto31/OrderCarWithDriverController.h
+++ ProkatAuto31/OrderCarWithDriverController.h
@@ -10,15 +10,19 @@
 #import "CarWithDriver.h"
 #import "RCTextField.h"
 
-@interface OrderCarWithDriverController : UITableViewController
+@interface OrderCarWithDriverController : UITableViewController <UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *carImageView;
 @property (weak, nonatomic) IBOutlet RCTextField *nameField;
 @property (weak, nonatomic) IBOutlet RCTextField *phoneField;
 @property (weak, nonatomic) IBOutlet RCTextField *emailField;
+@property (weak, nonatomic) IBOutlet UIImageView *captchaImageView;
 @property (weak, nonatomic) IBOutlet RCTextField *descriptionField;
 @property (weak, nonatomic) IBOutlet UIButton *sendOrderField;
+@property (weak, nonatomic) IBOutlet RCTextField *captchaField;
+@property (weak, nonatomic) IBOutlet UILabel *countryLabel;
+@property (weak, nonatomic) IBOutlet RCTextField *codeField;
 
 @property (strong, nonatomic) CarWithDriver *car;
 
