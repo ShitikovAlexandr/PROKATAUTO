ddb147600b838eded3df68d77b7284d355d4999b
alex <alex@MacBook-Air-alex.local>
2016-11-04 10:28:33 +0200

    add OrderDitailController

---
+++ ProkatAuto31/OrderDetailController.h
@@ -0,0 +1,13 @@
+//
+//  OrderDetailController.h
+//  ProkatAuto31
+//
+//  Created by alex on 03.11.16.
+//  Copyright © 2016 Asta.Mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderDetailController : UITableViewController
+
+@end
